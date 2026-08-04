6a8a065603343f8c2865de347b0e2bd25db5a348
JDE-Projects <289071709+JDE-Projects@users.noreply.github.com>
2026-08-04 11:27:44 -0500

    Bump version to v1.3.2

--- simple_firearm_logbook.py
+++ simple_firearm_logbook.py
@@ -30,7 +30,7 @@ from decimal import ROUND_HALF_UP, Decimal, InvalidOperation
 
 import webview
 
-APP_VERSION = "1.3.1"
+APP_VERSION = "1.3.2"
 GITHUB_OWNER = "JDE-Projects"
 GITHUB_REPO = "Simple-Firearm-Logbook"
 
