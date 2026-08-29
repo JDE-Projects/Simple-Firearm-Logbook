4e795444f26462ad517a70b91268aa0c496849e2
JDE-Projects <289071709+JDE-Projects@users.noreply.github.com>
2026-08-29 12:40:13 -0500

    Bump version to v1.6.1

--- simple_firearm_logbook.py
+++ simple_firearm_logbook.py
@@ -38,7 +38,7 @@ from PIL import Image, ImageOps
 # photo import everywhere in the app.
 Image.MAX_IMAGE_PIXELS = 32_000_000
 
-APP_VERSION = "1.6.0"
+APP_VERSION = "1.6.1"
 GITHUB_OWNER = "JDE-Projects"
 GITHUB_REPO = "Simple-Firearm-Logbook"
 
